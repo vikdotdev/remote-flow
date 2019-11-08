e2b2ed6b3dae25414086ea888f8ceb9d5458a072
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2019-11-08 15:51:39 +0200

    added basic registration with devise and changed user migration

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     email { Faker::Internet.email }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    password { 'passwd' }
     role { 'admin' }
     organization
   end
