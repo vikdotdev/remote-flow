02ce769c7495b28e4c06b777bcfb7aec06416d94
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2019-11-08 15:51:39 +0200

    added basic registration with devise and changed user migration

--- db/migrate/20191107180349_create_users.rb
+++ db/migrate/20191107180349_create_users.rb
@@ -4,10 +4,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :email
+      # t.string :email
       t.string :first_name
       t.string :last_name
-      t.string :password_digest
+      # t.string :password_digest
       t.string :role
       t.string :avatar
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     email { Faker::Internet.email }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    password { 'passwd' }
     role { 'admin' }
     # organization_id is overwritten in seeds.rb
     organization_id { 1 }
