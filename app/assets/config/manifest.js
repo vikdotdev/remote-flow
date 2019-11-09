a91b66d209011289da120d17fd233f4276dfddb6
vikdotdev <viktorhbk@gmail.com>
2019-11-09 19:08:12 +0200

    configure asset pipeline, implement frest template on login page.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,28 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../javascripts .js
+//
+//= link app.js
+//= link app-menu.js
+//= link components.js
+//= link footer.js
+//= link LivIconsEvo.defaults.js
+//= link LivIconsEvo.min.js
+//= link LivIconsEvo.tools.js
+//= link vendors.min.js
+//= link horizontal-menu.js
+//= link jquery.sticky.js
+//
+//= link authentication.css
+//= link bootstrap.css
+//= link bootstrap-extended.css
+//= link colors.css
+//= link components.css
+//= link dark-layout.css
+//= link semi-dark-layout.css
+//= link vendors.min.css
+//= link horizontal-menu.css
+//= link style.css
+//
+//= link login.png
+//= link auth-bg.jpg
