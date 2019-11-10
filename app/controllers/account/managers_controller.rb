41bb54f04345d7cb29028cc49d80c48c7fff14d4
met0ry <met0ry222@gmail.com>
2019-11-10 19:00:34 +0200

    added account namespace

---
+++ app/controllers/account/managers_controller.rb
@@ -0,0 +1,2 @@
+class Account::ManagersController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+
+  namespace :account do
+    resources :admins
+    resources :managers
+    resources :super_admin
+    resources :dashboard, only: [:index]
+  end
 end
