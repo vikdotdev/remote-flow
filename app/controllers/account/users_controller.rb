d279019ce04c670949af053dd1c227ce7301d1be
met0ry <met0ry222@gmail.com>
2019-11-15 13:06:47 +0200

    add index page example

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,2 +1,39 @@
 class Account::UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = resource
+  end
+
+  def edit
+    @user = resource
+  end
+
+  def update
+    @user = resource
+    if @user.update(users_params)
+      redirect_to account_user_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = resource
+    @user.destroy
+    redirect_to  account_user_path
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :role, :email, :avatar)
+  end
+
+  def resource
+    User.find(params[:id])
+  end
 end
