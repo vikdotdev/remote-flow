a14a797dba97a1e2d7fbb692ea1b098e7e60d133
met0ry <met0ry222@gmail.com>
2019-11-16 13:43:32 +0200

    add will_paginate

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,7 +1,7 @@
 class Account::UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   def show
