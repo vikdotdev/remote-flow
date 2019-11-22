9184827defe6e08a0369b53c684a96c30099400b
shamiwizard <leibyuk04mi@gmail.com>
2019-11-22 14:11:57 +0200

    fix after review

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,15 +19,4 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe 'the signin proccess', type: :feature do
-    let!(:user){ create(:user) }
-    it 'sign user in' do 
-      visit '/'
-      click_link("Log in")
-      fill_in 'Email',  with: user.email
-      fill_in 'Password',       with: 'passwd'
-      click_button 'Login'
-      expect(page).to have_current_path(root_path)
-    end
-  end
 end
