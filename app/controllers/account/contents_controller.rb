e69673a17c4809d69c78921aea13eb40700ba012
vikdotdev <viktorhbk@gmail.com>
2019-12-03 22:20:40 +0200

    move type path interpolation to 'content_type_path' helper, fix redundant filter

--- app/controllers/account/contents_controller.rb
+++ app/controllers/account/contents_controller.rb
@@ -16,9 +16,9 @@ class Account::ContentsController < Account::AccountController
   end
 
   def create
-    @content = collection.send(set_type.pluralize).new(contents_params)
+    @content = collection.new(contents_params)
     if @content.save
-      redirect_to account_organization_video_path(@content.organization, @content)
+      redirect_to send(content_type_path, @content.organization, @content)
       flash[:success] = 'Content successfully created.'
     else
       flash[:danger] = 'Failed to create content.'
@@ -29,7 +29,7 @@ class Account::ContentsController < Account::AccountController
   def update
     @content = resource
     if @content.update(contents_params)
-      redirect_to account_organization_video_path(@content)
+      redirect_to send(content_type_path, @content)
       flash[:success] = 'Content successfully updated.'
     else
       flash[:danger] = 'Failed to update content.'
@@ -44,7 +44,16 @@ class Account::ContentsController < Account::AccountController
     else
       flash[:danger] = 'Failed to delete content.'
     end
-    redirect_to account_organization_videos_path
+    redirect_to send(content_type_path(pluralize: true))
+  end
+
+  helper_method :content_type_path
+
+  def content_type_path(pluralize: false, prefix: nil)
+    type = params[:type].downcase
+    type = type.pluralize if pluralize
+    prefix = prefix ? "#{prefix}_" : ''
+    "#{prefix}account_organization_#{type}_path"
   end
 
   private
