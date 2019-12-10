6bd44d0bae64ce906d36b0589ed332f7a1e1eb96
shamiwizard <leibyuk04mi@gmail.com>
2019-12-10 23:53:09 +0200

    small fix after review

--- spec/features/users_export_csv_spec.rb
+++ spec/features/users_export_csv_spec.rb
@@ -5,15 +5,15 @@ RSpec.feature "Users export", type: :feature do
   describe 'to csv', type: :feature do
     let!(:user) { create(:user) }
 
-    it 'file downloud' do
+    it 'download file' do
       visit '/'
       click_link "Sign In"
       fill_in 'Email', with: user.email
       fill_in 'Password', with: 'passwd'
       click_button 'Login'
-      visit'account/users'
+      visit 'account/users'
       click_link 'Export to CSV'
-      expect( page.response_headers['Content-Disposition']).to include "filename=\"users-#{Date.today}.csv\""
+      expect( page.response_headers['Content-Disposition']).to include "users-#{Date.today}.csv"
     end
   end
 end
