f4115f47827a62df109e2fdce7da83d69b2ba101
vikdotdev <viktorhbk@gmail.com>
2019-12-11 20:45:15 +0200

    move notifier to model, move config to separate file, fix credentials bug

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,7 @@
-require 'slack-notifier'
-
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
 
-    debugger
     resource.role = User::ADMIN
     resource.organization_id = resource.organization.id if resource.organization.save
     resource.first_name = 'test_first_name'
@@ -14,14 +11,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     yield resource if block_given?
 
     if resource.persisted?
-      notifier = Slack::Notifier.new(
-        "https://hooks.slack.com/services/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",
-        channel: "#reflow",
-        username: "notifier"
-      )
-
-      notifier.ping "Organization #{resource.organization.name} was just created!"
-
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
@@ -32,10 +21,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
         respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
     else
-      debugger
-      resource.organization.delete
-      resource.delete
-
       clean_up_passwords resource
       set_minimum_password_length
       respond_with resource
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,5 @@
+require 'slack-notifier'
+
 class Organization < ApplicationRecord
   has_many :users, dependent: :destroy
   has_many :device_groups, dependent: :destroy
@@ -5,7 +7,7 @@ class Organization < ApplicationRecord
   has_many :channels, dependent: :destroy
   has_many :contents, dependent: :destroy
 
-  after_create :send_notification
+  after_create :send_email_notification, :send_slack_notification
 
   validates :name, presence: true,
                    uniqueness: true
@@ -16,7 +18,18 @@ class Organization < ApplicationRecord
 
   private
 
-  def send_notification
+  def send_slack_notification
+    config = YAML.load_file('config/initializers/slack.yml')
+    notifier = Slack::Notifier.new(
+      config[Rails.env]['token'],
+      channel: config[Rails.env]['channel'],
+      username: config[Rails.env]['username']
+    )
+
+    notifier.ping "Organization #{name} was just created!"
+  end
+
+  def send_email_notification
     SuperAdminMailer.new_organization_email(self).deliver_now
   end
 end
