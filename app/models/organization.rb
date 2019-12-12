443bd9b588b7bd07da5c61a89192675ac3f15671
vikdotdev <viktorhbk@gmail.com>
2019-12-12 23:42:59 +0200

    various minor fixes

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,3 @@
-require 'slack-notifier'
-
 class Organization < ApplicationRecord
   has_many :users, dependent: :destroy
   has_many :device_groups, dependent: :destroy
@@ -8,7 +6,8 @@ class Organization < ApplicationRecord
   has_many :contents, dependent: :destroy
   has_many :invites, dependent: :delete_all
 
-  after_create :send_email_notification, :send_slack_notification
+  after_create :send_email_notification
+  after_create :send_slack_notification
 
   validates :name, presence: true,
                    uniqueness: true
@@ -20,11 +19,10 @@ class Organization < ApplicationRecord
   private
 
   def send_slack_notification
-    config = Rails.application.config.SLACK_CONFIG
     notifier = Slack::Notifier.new(
-      config[:token],
-      channel: config[:channel],
-      username: config[:username]
+      SLACK_CONFIG[:token],
+      channel: SLACK_CONFIG[:channel],
+      username: SLACK_CONFIG[:username]
     )
 
     notifier.ping "Organization #{name} was just created!"
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,14 +38,6 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
 
-  module Slack
-    class Notifier
-      def ping(message)
-        message
-      end
-    end
-  end
-
   config.include Devise::Test::ControllerHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -75,3 +67,12 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+module Slack
+  class Notifier
+    def ping(message)
+      message
+    end
+  end
+end
+
