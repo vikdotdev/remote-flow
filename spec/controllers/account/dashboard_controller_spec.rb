72172f363c01943b68737c8cc9b722f19a059135
met0ry <met0ry222@gmail.com>
2019-12-15 20:57:56 +0200

    add test

--- spec/controllers/account/dashboard_controller_spec.rb
+++ spec/controllers/account/dashboard_controller_spec.rb
@@ -4,14 +4,20 @@ RSpec.describe Account::DashboardController, type: :controller do
   render_views
 
   let(:user) { create(:user) }
+  let!(:super_admin) { create(:user, :super_admin) }
 
   before do
-    sign_in user
+    sign_in super_admin
   end
 
   describe 'action test' do
     it 'should show index page' do
       get :index
+      expect(response).to be_successful
+    end
+
+    it 'should show analitics page' do
+      get :analitics
       expect(response).to be_successful
     end
   end
