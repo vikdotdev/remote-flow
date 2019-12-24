1286fd4d177b4a600e33b5465a71cc4eea4984d3
met0ry <met0ry222@gmail.com>
2019-12-24 22:47:47 +0200

    remove credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,6 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, '677154065603-8b612ihbgeaffj70mm7kgco7kpb84h0s.apps.googleusercontent.com', 'Yld4IM3AasaeIgsr3PvcyW7S', token_params: { parse: :json }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
