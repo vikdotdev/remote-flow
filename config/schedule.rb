8d0aefdfd6452c7bff3c9840fae852a76acf4c60
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2019-12-26 10:34:20 +0200

    fix tasks not running

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 set :output, "#{path}/log/cron.log"
+set :whenever_command, "bundle exec whenever"
+set :environment, ENV['RAILS_ENV']
 
 every :monday, at: '8:00am' do
-  SuperAdminMailer.send_statistic_about_new_users_and_organizations.deliver_now
+  runner 'SuperAdminMailer.send_statistic_about_new_users_and_organizations.deliver_now'
 end
--- lib/tasks/send_statistic_to_super_admin.rake
+++ lib/tasks/send_statistic_to_super_admin.rake
@@ -1,7 +1,7 @@
 namespace :send_mail do
   desc 'Send super_admin statistic about new users and organizations'
 
-  task :send_statistic_to_super_admin do
+  task send_statistic_to_super_admin: :environment do
     SuperAdminMailer.send_statistic_about_new_users_and_organizations.deliver_now
   end
 end
