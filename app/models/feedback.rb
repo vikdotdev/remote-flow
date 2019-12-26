ce4e819332b0a30225a8dfaada0cf28f90e38691
shamiwizard <leibyuk04mi@gmail.com>
2019-12-26 20:45:16 +0200

    small fix

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,5 @@
 class Feedback < ApplicationRecord
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true, length: { maximum: 250 }
 
   scope :by_creation_date, -> { order(created_at: :desc) }
