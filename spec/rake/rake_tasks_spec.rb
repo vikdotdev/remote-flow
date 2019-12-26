6daae5af90167404bbff1715177c66f0e7709813
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2019-12-26 11:02:21 +0200

    add specs

--- spec/rake/rake_tasks_spec.rb
+++ spec/rake/rake_tasks_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
+require 'rake'
 Rails.application.load_tasks
 
-RSpec.describe TestRakeTasks do
+describe 'send_mail namespace rake tasks' do
+  describe 'send_mail:send_statistic_to_super_admin' do
+    Rake::Task.task_defined?("send_mail:send_statistic_to_super_admin")
+  end
 end
