a15b803cd2306b5dcbc70619ffb8e77951a53f81
met0ry <met0ry222@gmail.com>
2019-12-27 22:14:15 +0200

    add test and fix form

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,17 +59,18 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.find_or_initialize_by(email: data['email'])
-
+    data = access_token[:info]
+    user = User.find_by(email: data[:email])
     unless user
-      user = User.create(first_name: data['first_name'],
-      last_name: data['last_name'],
-      email: data['email'],
+      password = Devise.friendly_token[0,20]
+      user = User.create(first_name: data[:first_name],
+      last_name: data[:last_name],
+      email: data[:email],
       role: ADMIN,
-      password: Devise.friendly_token[0,20],
-      google_token: access_token.credentials.token,
-      google_refresh_token: access_token.credentials.refresh_token
+      password: password,
+      password_confirmation: password,
+      google_token: access_token[:credentials][:token],
+      google_refresh_token: access_token[:credentials][:refresh_token]
       )
     end
     user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { create(:user) }
+  let!(:user) { create(:user) }
+  let!(:organization) { create(:organization) }
+  let(:auth) { OmniAuth::AuthHash.new(
+    info: {
+      email: "jessepinkman@mountainmantechnologies.com",
+      first_name: "Jesse",
+      last_name: "Spevack"
+    },
+    credentials: {
+      token: "abcdefg12345",
+      refresh_token: "12345abcdefg",
+    }
+  )}
 
   describe 'factory specs' do
     it 'has factory' do
@@ -18,6 +30,21 @@ RSpec.describe User, type: :model do
     it 'has valid email' do
       user.email = 'some@valid.em'
       expect(user).to be_valid
+    end
+  end
+
+  describe 'google login' do
+    it "creates or updates itself from an oauth hash" do
+      user = User.from_omniauth(auth)
+      user.organization_id = organization.id
+      user.save
+      new_user = User.last
+
+      expect(new_user.email).to eq("jessepinkman@mountainmantechnologies.com")
+      expect(new_user.first_name).to eq("Jesse")
+      expect(new_user.last_name).to eq("Spevack")
+      expect(new_user.google_token).to eq("abcdefg12345")
+      expect(new_user.google_refresh_token).to eq("12345abcdefg")
     end
   end
 end
