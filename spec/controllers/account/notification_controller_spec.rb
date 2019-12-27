d30787a177af6dc03492a2aec9a77113954695ed
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2019-12-27 08:34:40 +0200

    change test

--- spec/controllers/account/notification_controller_spec.rb
+++ spec/controllers/account/notification_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Account::NotificationController, type: :controller do
   let!(:user) { create(:user, first_name: 'Bob') }
-  let!(:notification) { create(:notification, notification_type: Notification::USER_ADDED, notificable: user, user_id: user.id) }
+  let!(:notification) { create(:notification, notification_type: Notification::NOTIFICATION_TYPES.sample, notificable: user, user_id: user.id) }
 
   describe 'mark_all_as_read' do
     it 'marks all notifications as read' do
