16c816be20ab4cd96df50a91cfafbd9022f3d64f
met0ry <met0ry222@gmail.com>
2019-12-30 23:40:08 +0200

    add tests

--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe AdminMailer, type: :mailer do
-  
+  describe 'send notification email' do
+    let!(:admin) { create(:user, :admin, organization: organization) }
+    let!(:organization) { create(:organization) }
+    let!(:channel) { create(:channel, organization: organization) }
+    let!(:mail) { AdminMailer.delete_channel_email(channel, organization) }
+
+    it 'deliver success' do
+      expect { channel.destroy }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+
+    it 'with admins emails' do
+       expect(mail.to).to match_array(organization.users.admins.pluck(:email))
+    end
+
+    it 'with correct subject' do
+      expect(mail.subject).to eq('Channel was deleted')
+    end
+  end
 end
