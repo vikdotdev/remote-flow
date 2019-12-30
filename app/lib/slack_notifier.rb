3dbc21b4ffe54e8a5c78432d7b6828d0a962cfa7
vikdotdev <viktorhbk@gmail.com>
2019-12-30 19:45:12 +0200

    make slackbot notifier toggleable

---
+++ app/lib/slack_notifier.rb
@@ -0,0 +1,13 @@
+class SlackNotifier
+  def self.ping(message)
+    if SLACK_CONFIG[:enabled]
+      notifier = Slack::Notifier.new(
+        SLACK_CONFIG[:token],
+        channel: SLACK_CONFIG[:channel],
+        username: SLACK_CONFIG[:username]
+      )
+
+      notifier.ping message
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 module RemoteFlow
   class Application < Rails::Application
     config.load_defaults 6.0
-    config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
+    config.autoload_paths += %w(#{config.root}/app/models/ckeditor #{config.root}/app/lib)
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
 
