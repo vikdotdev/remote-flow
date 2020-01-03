aa76a553a0d8d0539b60a57f29cd668d3a21e3f7
met0ry <met0ry222@gmail.com>
2020-01-03 14:19:11 +0200

    another fix

--- app/controllers/account/feedbacks_controller.rb
+++ app/controllers/account/feedbacks_controller.rb
@@ -2,16 +2,26 @@ class Account::FeedbacksController < Account::AccountController
   before_action :require_super_admin_only!, only: [:index, :destroy]
 
   def index
-    @feedbacks = Feedback.by_creation_date
+    @feedbacks = Feedback.with_deleted.by_creation_date
   end
 
   def destroy
-    @feedback = Feedback.find(params[:id])
-    if @feedback.destroy
-      flash[:success] = 'Feedback successfully deleted.'
+    @feedback = Feedback.with_deleted.find(params[:id])
+    if @feedback.deleted_at == nil
+      @feedback.destroy
+      flash[:info] = 'Feedback was put on delete queue'
     else
-      flash[:danger] = 'Failed to delete feedback.'
+      @feedback.really_destroy!
+      flash[:success] = 'Feedback successfully deleted.'
     end
+    redirect_to account_feedbacks_path
+  end
+
+  def restore
+    @feedback = Feedback.with_deleted.find(params[:id])
+    @feedback.restore
+    flash[:success] = 'Feedback successfully restored.'
+
     redirect_to account_feedbacks_path
   end
 end
