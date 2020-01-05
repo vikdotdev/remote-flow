eb424eea7b65d1caf4e2a4c9221a5a4e3a168e7d
vikdotdev <viktorhbk@gmail.com>
2020-01-05 15:23:04 +0200

    display thumbnails for presentations

--- app/uploaders/screenshot_uploader.rb
+++ app/uploaders/screenshot_uploader.rb
@@ -1,5 +1,11 @@
 class ScreenshotUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
   storage :file
+
+  version :thumb do
+    process resize_to_fit: [200, 200]
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
