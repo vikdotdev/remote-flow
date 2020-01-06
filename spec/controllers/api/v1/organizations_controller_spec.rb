cefa6c3a99d071413ecbf09375091712a9bda9d4
Taras <swex99@gmail.com>
2020-01-06 19:46:03 +0200

    Little Fix With OrganizationsController

--- spec/controllers/api/v1/organizations_controller_spec.rb
+++ spec/controllers/api/v1/organizations_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::OrganizationsController, type: :controller do
-
   let!(:organization) { create(:organization) }
   let!(:admin) { create(:user, :admin) }
 
@@ -11,43 +10,11 @@ RSpec.describe Api::V1::OrganizationsController, type: :controller do
       get :show
       expect(response).to have_http_status(200)
     end
-  end
 
-  describe 'POST #create' do
-    it 'create new organization' do
-      post :create, params: { organization: { name: Faker::Commerce.name, logo: 'logo.png' } }
-      expect(response).to have_http_status(200)
-    end
-
-    it 'create new organization with already exist name' do
-      post :create, params: { organization: { name: organization.name, logo: 'logo.png' } }
-      expect(response).to have_http_status(422)
-    end
-  end
-
-  describe 'PUT #update' do
-    it 'update organization`s name by token' do
-      request.headers['token'] = organization.token
-      put :update, params: { organization: { name: Faker::Commerce.name } }
-      expect(response).to have_http_status(200)
-    end
-
-    it 'update organization`s name without token' do
-      put :update, params: { organization: { name: Faker::Commerce.name } }
-      expect(response).to have_http_status(404)
-    end
-  end
-
-  describe 'GET #destroy' do
-    it 'remove organization by token' do
-      request.headers['token'] = organization.token
-      delete :destroy
-      expect(response).to have_http_status(200)
-    end
-
-    it 'remove organization without token' do
-      delete :destroy
-      expect(response).to have_http_status(404)
+    it 'return error by invalid token' do
+      request.headers['token'] = organization.token + 'Ruby'
+      get :show
+      expect(response).to have_http_status(400)
     end
   end
 end
