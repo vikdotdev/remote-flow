6c8157a8ce9005729aae80c5ff37bf1fc165d958
vikdotdev <viktorhbk@gmail.com>
2020-01-08 14:51:24 +0200

    set select placeholder

--- app/assets/javascripts/account/channel_content_select2.js
+++ app/assets/javascripts/account/channel_content_select2.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-  $('.js-select2-contents').select2({ width: '100%' });
+  $('.js-select2-contents').select2({
+    placeholder: '-- Select Content --',
+    width: '100%'
+  });
 });
 
 
