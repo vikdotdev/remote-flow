ea87439021a225d1e497ccc82a68e4567b5a7213
vikdotdev <viktorhbk@gmail.com>
2020-01-10 20:07:54 +0200

    add icon uploader for channels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_134524) do
+ActiveRecord::Schema.define(version: 2020_01_10_174807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_01_02_134524) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "organization_id", null: false
+    t.string "icon"
     t.index ["organization_id"], name: "index_channels_on_organization_id"
   end
 
