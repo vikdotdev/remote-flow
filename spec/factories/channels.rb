57bcbd07c65441d6d4333c1aa7a4d0080731ba67
vikdotdev <viktorhbk@gmail.com>
2020-01-10 20:12:08 +0200

    add with_icons trait for channel factory

--- spec/factories/channels.rb
+++ spec/factories/channels.rb
@@ -2,5 +2,14 @@ FactoryBot.define do
   factory :channel do
     name { Faker::App.name }
     organization
+
+    trait :with_icon do
+      icon do
+        Rack::Test::UploadedFile.new(
+          Rails.root + 'vendor/assets/images/device_icons/default.svg',
+          'image/svg'
+        )
+      end
+    end
   end
 end
