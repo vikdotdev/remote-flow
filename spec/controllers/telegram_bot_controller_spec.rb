dd7a4c16fae0279570160b0b00c050236064a29a
shamiwizard <leibyuk04mi@gmail.com>
2020-01-13 18:53:49 +0200

    made one test

--- spec/controllers/telegram_bot_controller_spec.rb
+++ spec/controllers/telegram_bot_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
+require 'telegram/bot/updates_controller/rspec_helpers'
 
-RSpec.describe TelegramBotController, type: :controller do
-
+RSpec.describe TelegramBotController, telegram_bot: :rails do
+  describe '#start' do
+    it 'response return' do
+      expect{ dispatch_command :start }.to respond_with_message 'Hi. Entry you /email exemple@mail.com and /password exemple'
+      debugger
+    end
+  end
 end
