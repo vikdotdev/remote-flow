44e97675b8174ac2d900acfe5f2dc4f11f2390c9
met0ry <met0ry222@gmail.com>
2020-01-17 22:09:58 +0200

    fix

--- app/controllers/account/search_controller.rb
+++ app/controllers/account/search_controller.rb
@@ -1,12 +1,5 @@
 class Account::SearchController < Account::AccountController
   def search
-    byebug
     @search_results = Elasticsearch::Model.search(params[:q]).page(params[:page]).per(15)
   end
-  
-  # private
-  #
-  # def search_params
-  #   params.require(:search).permit(:q)
-  # end
 end
