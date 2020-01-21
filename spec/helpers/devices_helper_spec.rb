0101350fabed3387b8a726529dd2fe185d57f6b2
vikdotdev <viktorhbk@gmail.com>
2020-01-21 15:16:29 +0200

    add background specs

--- spec/helpers/devices_helper_spec.rb
+++ spec/helpers/devices_helper_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe DevicesHelper, type: :helper do
-  it 'returns image tag with fallback svg if arg is blank' do
-    expect(icon('')).to eq(image_tag asset_path('fallback/default_channel_icon.svg'))
+  let!(:organization) { create(:organization) }
+  let!(:background) { create(:background, organization: organization) }
+  let!(:device) { create(:device, organization: organization) }
+
+  describe 'icon helper' do
+    it 'returns image tag with fallback svg if arg is blank' do
+      expect(icon('')).to eq(image_tag asset_path('fallback/default_channel_icon.svg'))
+    end
+
+    it 'returns image tag with chosen svg' do
+      expect(icon('channel_icons/camera.svg')).to eq(image_tag asset_path('channel_icons/camera.svg'))
+    end
   end
 
-  it 'returns image tag with chosen svg' do
-    expect(icon('channel_icons/camera.svg')).to eq(image_tag asset_path('channel_icons/camera.svg'))
+  describe 'background helper' do
+    it 'returns random existing image path' do
+      path = Rails.root.join('public').to_s + random_background(device)
+      expect(Pathname.new(path).exist?).to be true
+    end
   end
 end
 
--- spec/models/background_spec.rb
+++ spec/models/background_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Background, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:background) { create(:background) }
+
+  describe 'factory specs' do
+    it 'has factory' do
+      expect(background).to be_persisted
+    end
+  end
+
+  describe 'validation specs' do
+    it 'has image' do
+      expect(background.image).not_to be_nil
+    end
+  end
 end
