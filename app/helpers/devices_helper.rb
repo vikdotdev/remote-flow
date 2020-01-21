d17bdac91f96f8d043caab044c8a165e7ff492c4
vikdotdev <viktorhbk@gmail.com>
2020-01-21 15:07:46 +0200

    fix random background, add spec

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -3,4 +3,9 @@ module DevicesHelper
     path = 'fallback/default_channel_icon.svg' if path.blank?
     image_tag asset_path(path)
   end
+
+  def random_background(device)
+    bg = device.organization.backgrounds.order('random()').limit(100)
+    asset_path(bg.first&.image ? bg.first.image : '')
+  end
 end
