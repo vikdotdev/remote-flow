d3a622161307822a10854646268fcb8fef802b88
vikdotdev <viktorhbk@gmail.com>
2020-01-21 16:06:48 +0200

    add jest and enzyme for react testing

---
+++ spec/javascript/backgrounds.test.js
@@ -0,0 +1,10 @@
+import moment from 'moment';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Background from './Background'
+
+test('1 + 1 equals 2', () => {
+  const background = shallow(<Background />);
+});
+
